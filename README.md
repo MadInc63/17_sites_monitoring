6edd8282953c52a4e047272f9c4a31ba1293295e
MadInc63 <MadInc63@gmail.com>
2017-12-11 10:45:20 +0400

    first release

--- README.md
+++ README.md
@@ -1,6 +1,37 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+A script from the specified text file with a list of URLs, checks the existence of the URL and the expiration date is longer than 30 days.
+
+# How to use
+
+The script requires the installed Python interpreter version 3.5 To call the help, run the script with the -h or --help option.
+
+```Bash
+$python check_sites_health.py
+usage: check_sites_health.py [-h] path
+check_sites_health.py: error: the following arguments are required: path
+```
+
+To start executing the script, you must specify the path to the file with the URL list.
+
+```Bash
+$python check_sites_health.py url.txt
+http://yandex.ru
+Responce from URL: <Response [200]>
+Expiration date: 2018-09-30 21:00:00
+Days before expiration: 293
+----------------------------------------------------------
+http://www.google.ru
+Responce from URL: <Response [200]>
+Expiration date: 2018-03-04 21:00:00
+Days before expiration: 83
+----------------------------------------------------------
+http://mail.ru
+Responce from URL: <Response [200]>
+Expiration date: 2018-09-30 21:00:00
+Days before expiration: 293
+----------------------------------------------------------
+```
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,42 @@
-def load_urls4check(path):
-    pass
+import argparse
+import requests
+from whois import whois
+from urllib.parse import urlparse
+from datetime import datetime
 
-def is_server_respond_with_200(url):
-    pass
+
+def arg_parser():
+    parse = argparse.ArgumentParser(description='URL health checker')
+    parse.add_argument('path', type=str, help='path to open file with url')
+    return parse.parse_args()
+
+
+def load_urls4check(path):
+    with open(path, encoding='utf-8') as urls_file:
+        for url_string in urls_file:
+            yield url_string.strip()
+
+
+def is_server_respond_with_200(verifiable_url):
+    responce = requests.get(verifiable_url)
+    return responce
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    domain_info = whois(domain_name)
+    return domain_info.expiration_date
+
 
 if __name__ == '__main__':
-    pass
+    arg = arg_parser()
+    for url in load_urls4check(arg.path):
+        domain_expiration_date = get_domain_expiration_date(urlparse(url)[1])
+        current_date = datetime.now()
+        delta = domain_expiration_date - current_date
+        if delta.days > 30:
+            print(url)
+            print('Responce '
+                  'from URL: {}'.format(is_server_respond_with_200(url)))
+            print('Expiration date: {}'.format(domain_expiration_date))
+            print('Days before expiration: {}'.format(delta.days))
+            print('----------------------------------------------------------')
