a18b74fed6d9c79c891b4dec785c52607ea8723d
MadInc63 <MadInc63@gmail.com>
2017-12-18 09:01:33 +0400

    add requirements.txt fix spelling mistake rename function corrected the verification of non-existent domains

--- check_sites_health.py
+++ check_sites_health.py
@@ -5,7 +5,7 @@ from urllib.parse import urlparse
 from datetime import datetime
 
 
-def arg_parser():
+def parse_args():
     parse = argparse.ArgumentParser(description='URL health checker')
     parse.add_argument('path', type=str, help='path to open file with url')
     return parse.parse_args()
@@ -18,8 +18,12 @@ def load_urls4check(path):
 
 
 def is_server_respond_with_200(verifiable_url):
-    responce = requests.get(verifiable_url)
-    return responce
+    try:
+        response = requests.get(verifiable_url)
+        if response.status_code == 200:
+            return True
+    except requests.exceptions.ConnectionError:
+        return False
 
 
 def get_domain_expiration_date(domain_name):
@@ -28,15 +32,19 @@ def get_domain_expiration_date(domain_name):
 
 
 if __name__ == '__main__':
-    arg = arg_parser()
+    arg = parse_args()
     for url in load_urls4check(arg.path):
-        domain_expiration_date = get_domain_expiration_date(urlparse(url)[1])
-        current_date = datetime.now()
-        delta = domain_expiration_date - current_date
-        if delta.days > 30:
-            print(url)
-            print('Responce '
-                  'from URL: {}'.format(is_server_respond_with_200(url)))
-            print('Expiration date: {}'.format(domain_expiration_date))
-            print('Days before expiration: {}'.format(delta.days))
-            print('----------------------------------------------------------')
+        response_200 = is_server_respond_with_200(url)
+        if response_200:
+            domain_expiration_date = get_domain_expiration_date(
+                urlparse(url)[1]
+            )
+            current_date = datetime.now()
+            delta = domain_expiration_date - current_date
+            if delta.days > 30:
+                print(url)
+                print('Responce '
+                      'from URL: {}'.format(response_200))
+                print('Expiration date: {}'.format(domain_expiration_date))
+                print('Days before expiration: {}'.format(delta.days))
+                print('------------------------------------------------------')
